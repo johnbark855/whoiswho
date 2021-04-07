39ca1d721776b3b7eda2caf54b0efafb16c16178
Jeff Woda <wodahouse@gmail.com>
2021-04-06 21:26:46 -0400

    added AWS Authentication

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
+// const MongoStore = require("connect-mongo");
 const mongoose = require("mongoose");
 
 const passport = require("./passport/setup");
@@ -8,26 +8,28 @@ const auth = require("./routes/auth");
 
 const app = express();
 const PORT = 5000;
-const MONGO_URI = "mongodb://127.0.0.1:27017/userdb";
+// const MONGO_URI = "mongodb://127.0.0.1:27017/userdb";
 
-mongoose
-    .connect(MONGO_URI, { useNewUrlParser: true })
-    .then(console.log(`MongoDB connected ${MONGO_URI}`))
-    .catch(err => console.log(err));
+// mongoose
+//   .connect(MONGO_URI, { useNewUrlParser: true })
+//   .then(console.log(`MongoDB connected ${MONGO_URI}`))
+//   .catch((err) => console.log(err));
 
 // Bodyparser middleware, extended false does not allow nested payloads
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb");
+
 // Express Session
-app.use(
-    session({
-        secret: "very secret this is",
-        resave: false,
-        saveUninitialized: true,
-        store: new MongoStore({ mongooseConnection: mongoose.connection })
-    })
-);
+// app.use(
+//   session({
+//     secret: "very secret this is",
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
+//   })
+// );
 
 // Passport middleware
 app.use(passport.initialize());
